8c9065411eb83447fe7cdb61e53d565a93758373
kkorona <high1uck@pusan.ac.kr>
2019-02-11 17:21:48 +0900

    added comment

--- learn.cpp
+++ learn.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 const int N = 9;
+const string LINE("-------------------------------------------------------------\n");
 
+// Return 1d distance
 int checkReward(int answer, int response) {
 	int ret = -(abs(answer - response));
 	return ret;
 }	
 
+// Generate randomly distributed query table
 vector < pair < int,int > > stateGen() {
 	bool check[N+1][N+1];
 	vector < pair < int,int > > ret;
@@ -30,6 +33,7 @@ vector < pair < int,int > > stateGen() {
 	return ret;
 }
 
+// generated randomly shuffled vector
 vector < int > random_Permuatation() {
 	srand(time(NULL));
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -45,13 +49,16 @@ vector < int > random_Permuatation() {
 
 int main() {
 
+	// Set File I/O
 	fstream out;
 	out.open("result.txt",fstream::out);
 	
+	// answerTable[i][j] = i*j
+	// valueTable[i][j][k] = currently estimated q((i,j), k)
+	// predictionTable[i][j] = (k that q(i,j) = valueTable[i][j][k])
 	int answerTable[N+1][N+1], valueTable[N+1][N+1][N*N+1], predictionTable[N+1][N+1];
 	
-	const string LINE("-------------------------------------------------------------\n");
-	
+	// init
 	for(int i=1; i<=N; i++) {
 		for(int j=1; j<=N; j++) {
 			for(int k=1; k<=N*N; k++) {
@@ -62,12 +69,15 @@ int main() {
 		}
 	}
 	
+	// Start evolution
 	int generation = 1;
 	while(1) {
 		vector < pair < int,int > > query = stateGen();
 		out << "Generation " << generation << "\n\n";
 		int answerCount = 0; 
 		cout << generation << '\n';
+
+		// complete table with given query
 		for(auto& v : query) {
 			int x = v.first, y=v.second;
 			int target = predictionTable[x][y];
@@ -82,6 +92,7 @@ int main() {
 			}
 			//cout << endl;
 			
+			// Check reward
 			valueTable[x][y][target] = checkReward(answerTable[x][y],target);
 
 			if(valueTable[x][y][target] == 0) {
@@ -91,6 +102,7 @@ int main() {
 			predictionTable[x][y] = target;
 		}
 
+		// print table
 		for(int i=1; i<=9; i++) {
 			for(int j=1; j<=9; j++) {
 				out << predictionTable[i][j] << " ";
@@ -98,10 +110,13 @@ int main() {
 			out << '\n';
 		}
 
+		// If all answers are correct
 		if(answerCount == N*N) {
 			out << "\nLearning finished" << '\n';
 			break;
 		}
+
+		// Evaluate accuracy
 		out << "Accuracy: " << answerCount << "/81\n" << LINE;
 		generation++;
 	}
